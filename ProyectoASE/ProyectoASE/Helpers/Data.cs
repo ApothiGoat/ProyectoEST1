3fbb7f53135ea5c1ca25c85033a5cb77db1a5245
SARUCA1 <santiagoruizc@gmail.com>
2022-04-25 18:04:03 -0600

    Arbol AVL

--- ProyectoASE/ProyectoASE/Helpers/Data.cs
+++ ProyectoASE/ProyectoASE/Helpers/Data.cs
@@ -7,5 +7,18 @@ namespace ProyectoASE.Helpers
 {
     public class Data
     {
+        private static Data _instance = null;
+
+        public static Data Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new Data();
+                }
+                return _instance;
+            }
+        }
     }
 }
