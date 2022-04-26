d9772ddf1d402ffc8baa94ac9d9d4ab51e360cba
Alejandro Gil <alejandro115935@gmail.com>
2022-04-25 18:36:28 -0600

    delegado

---
+++ ProyectoASE/ProyectoASE/Delegate.cs
@@ -0,0 +1,37 @@
+﻿using ProyectoASE.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoASE
+{
+    public class Delegate
+    {
+
+        public delegate int Comparar<T>(T a, T b);
+
+        public class Comparar
+        {
+            public static int CompID(PacientesModel a, PacientesModel b)
+            {
+                if (a.Name != b.Name)
+                {
+                    if (a.Name.CompareTo(b.Name) < 0)
+                    {
+                        return -1;
+                    }
+                    else
+                    {
+                        return 1;
+                    }
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+
+        }
+    }
+}
