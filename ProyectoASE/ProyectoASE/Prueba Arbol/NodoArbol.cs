a9d4c437c47f474e6e0c83f8ceaeffc4bbd1f704
Edryan López <edryanandre@gmail.com>
2022-04-30 16:58:58 -0600

    Prueba Nodo

---
+++ ProyectoASE/ProyectoASE/Prueba Arbol/NodoArbol.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoASE.Prueba_Arbol
+{
+    public class NodoArbol<T>
+    {
+        public T Value { get; set; }
+
+        public NodoArbol<T>? Primero { get; set; }
+
+        public NodoArbol<T>? Segundo { get; set; }
+
+        public int Balance { get; set; }
+
+        public NodoArbol(T Value)
+        {
+            this.Value = Value;
+            Primero = null;
+            Segundo = null;
+
+        }
+    }
+}
